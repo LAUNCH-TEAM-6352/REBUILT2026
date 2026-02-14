7662a514e17493bb01d4f87a04e4f86029074ac7
Justin Nguyen <you@exampliamaalt921@gmail.com>
2026-02-14 16:18:55 -0700

    Remove redundant code, remove recursive code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -16,15 +16,16 @@ import com.ctre.phoenix6.signals.SensorDirectionValue;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.DashboardConstants;
 
 public class Intake extends SubsystemBase
 {
 
-    private static TalonFX intakeMotor = new TalonFX(Constants.IntakeConstants.INTAKE_MOTOR_CHANNEL);
-    private static TalonFX pivotMotor = new TalonFX(Constants.IntakeConstants.PIVOT_MOTOR_CHANNEL);
+    private static TalonFX intakeMotor = new TalonFX(IntakeConstants.INTAKE_MOTOR_CHANNEL);
+    private static TalonFX pivotMotor = new TalonFX(IntakeConstants.PIVOT_MOTOR_CHANNEL);
 
-    public static CANcoder pivotEncoder = new CANcoder(Constants.IntakeConstants.PIVOT_ENCODER_CHANNEL);
+    public static CANcoder pivotEncoder = new CANcoder(IntakeConstants.PIVOT_ENCODER_CHANNEL);
 
     private boolean isDeployed = false;
 
@@ -32,22 +33,22 @@ public class Intake extends SubsystemBase
     public Intake()
     {
         var intakeConfigs = new TalonFXConfiguration();
-        intakeConfigs.MotorOutput.Inverted = Constants.IntakeConstants.INTAKE_MOTOR_INVERTED_VALUE;
+        intakeConfigs.MotorOutput.Inverted = IntakeConstants.INTAKE_MOTOR_INVERTED_VALUE;
         intakeMotor.getConfigurator().apply(intakeConfigs);
 
         var pivotConfigs = new TalonFXConfiguration();
-        pivotConfigs.MotorOutput.Inverted = Constants.IntakeConstants.PIVOT_MOTOR_INVERTED_VALUE;
+        pivotConfigs.MotorOutput.Inverted = IntakeConstants.PIVOT_MOTOR_INVERTED_VALUE;
         pivotConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
         pivotConfigs.Feedback.FeedbackRemoteSensorID = pivotEncoder.getDeviceID();
-        pivotConfigs.Slot0.kP = Constants.IntakeConstants.IntakekP;
-        pivotConfigs.Slot0.kI = Constants.IntakeConstants.IntakekI;
-        pivotConfigs.Slot0.kD = Constants.IntakeConstants.IntakekD;
-        pivotConfigs.MotorOutput.PeakForwardDutyCycle = Constants.IntakeConstants.PIVOT_SPEED;
-        pivotConfigs.MotorOutput.PeakReverseDutyCycle = -Constants.IntakeConstants.PIVOT_SPEED;
+        pivotConfigs.Slot0.kP = IntakeConstants.IntakekP;
+        pivotConfigs.Slot0.kI = IntakeConstants.IntakekI;
+        pivotConfigs.Slot0.kD = IntakeConstants.IntakekD;
+        pivotConfigs.MotorOutput.PeakForwardDutyCycle = IntakeConstants.PIVOT_SPEED;
+        pivotConfigs.MotorOutput.PeakReverseDutyCycle = -IntakeConstants.PIVOT_SPEED;
         pivotConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-        pivotConfigs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = Constants.IntakeConstants.DEPLOYED_POSITION;
+        pivotConfigs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = IntakeConstants.DEPLOYED_POSITION;
         pivotConfigs.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
-        pivotConfigs.SoftwareLimitSwitch.ReverseSoftLimitThreshold = Constants.IntakeConstants.STOW_POSITION;
+        pivotConfigs.SoftwareLimitSwitch.ReverseSoftLimitThreshold = IntakeConstants.STOW_POSITION;
         pivotConfigs.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
         pivotMotor.getConfigurator().apply(pivotConfigs);
 
@@ -74,50 +75,34 @@ public class Intake extends SubsystemBase
     // Pivot the intake into "stowed" position
     public void stow()
     {
-        pivotToPosition(Constants.IntakeConstants.STOW_POSITION);
+        pivotToPosition(IntakeConstants.STOW_POSITION);
     }
 
     // Pivot the intake into "deployed" position
     public void deploy()
     {
-        pivotToPosition(Constants.IntakeConstants.DEPLOYED_POSITION);
+        pivotToPosition(IntakeConstants.DEPLOYED_POSITION);
     }
 
     // Pivot the intake into "partially deployed" position
     public void partialDeploy()
     {
-        pivotToPosition(Constants.IntakeConstants.PARTIALLY_DEPLOYED_POSITION);
+        pivotToPosition(IntakeConstants.PARTIALLY_DEPLOYED_POSITION);
     }
 
     // Ensure intake is pivoted into "deployed" position, then run the intake motor to pull fuel into the robot
     public void intake()
     {
-        if (!isDeployed)
-        {
-            deploy();
-            intake();
-        }
-        else
-        {
             setIntakeSpeed(
-                SmartDashboard.getNumber(DashboardConstants.INTAKE_KEY, Constants.IntakeConstants.INTAKE_SPEED));
-        }
+                SmartDashboard.getNumber(DashboardConstants.INTAKE_KEY, IntakeConstants.INTAKE_SPEED));
     }
 
     // Ensure intake is pivoted into "deployed" position, then run the intake motor in reverse to "dump" fuel out of
     // the robot
     public void eject()
     {
-        if (!isDeployed)
-        {
-            deploy();
-            eject();
-        }
-        else
-        {
             setIntakeSpeed(
-                SmartDashboard.getNumber(DashboardConstants.EJECT_KEY, Constants.IntakeConstants.EJECT_SPEED));
-        }
+                SmartDashboard.getNumber(DashboardConstants.EJECT_KEY, IntakeConstants.EJECT_SPEED));
     }
 
     // Stop the intake motor
@@ -132,7 +117,7 @@ public class Intake extends SubsystemBase
         // This method will be called once per scheduler run
         double currentPivotPosition = pivotEncoder.getPosition().getValueAsDouble();
 
-        if (currentPivotPosition > Constants.IntakeConstants.DEPLOYED_POSITION - 10)
+        if (currentPivotPosition > IntakeConstants.DEPLOYED_POSITION - 10)
         {
             isDeployed = true;
         }
