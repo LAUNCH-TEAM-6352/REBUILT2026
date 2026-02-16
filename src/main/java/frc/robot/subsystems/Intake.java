4a7cabf38eadeb989ba8cc7d339e54125607b739
AmazingMaisey <donickmaisey@gmail.com>
2026-02-16 11:41:01 -0700

    Cleanup code, change hopper indexer/lower indexer to conveyor, add method commands where they may be needed.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -9,6 +9,7 @@ import static edu.wpi.first.units.Units.Degrees;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
@@ -16,6 +17,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DashboardConstants;
 import frc.robot.Constants.IntakeConstants;
@@ -28,7 +30,7 @@ public class Intake extends SubsystemBase
 
     public static CANcoder pivotEncoder = new CANcoder(IntakeConstants.PIVOT_ENCODER_CHANNEL);
 
-    PositionDutyCycle pivotPositionControl = new PositionDutyCycle(0).withSlot(0);
+    private final PositionVoltage positionVoltage = new PositionVoltage(0);
 
     /** Creates a new Intake. */
     public Intake()
@@ -69,40 +71,67 @@ public class Intake extends SubsystemBase
         intakeMotor.set(speed);
     }
 
-    // Pivot the intake to a specific position in degrees
+
     private void pivotToPosition(double position)
     {
-        pivotMotor.setControl(pivotPositionControl.withPosition(Degrees.of(position)));
+        pivotMotor.setControl(positionVoltage.withPosition(position));
     }
 
-    // Pivot the intake into "stowed" position
+    public Command stowCommand()
+    {
+        return runOnce(this::stow);
+    }
+
     public void stow()
     {
         pivotToPosition(SmartDashboard.getNumber(DashboardConstants.STOW_KEY, IntakeConstants.STOW_POSITION));
     }
 
-    // Pivot the intake into "deployed" position
+    public Command deployCommand()
+    {
+        return runOnce(this::deploy);
+    }
+
     public void deploy()
     {
         pivotToPosition(SmartDashboard.getNumber(DashboardConstants.DEPLOY_KEY, IntakeConstants.DEPLOYED_POSITION));
     }
 
-    // Pivot the intake into "partially deployed" position
+    public Command partialDeployCommand()
+    {
+        return runOnce(this::partialDeploy);
+    }
+
     public void partialDeploy()
     {
         pivotToPosition(SmartDashboard.getNumber(DashboardConstants.PARTIALLY_DEPLOY_KEY,
             IntakeConstants.PARTIALLY_DEPLOYED_POSITION));
     }
 
-    // Ensure intake is pivoted into "deployed" position, then run the intake motor to pull fuel into the robot
+    // Intended for use with a press-and-hold binding
+    public Command intakeThenStopCommand()
+    {
+        return startEnd(this::intake, this::stop);
+    }
+
+    // Intended for use in auto (Only starts the intake, does not stop automatically)
+    public Command intakeCommand()
+    {
+        return runOnce(this::intake);
+    }
+
     public void intake()
     {
         setIntakeSpeed(
             SmartDashboard.getNumber(DashboardConstants.INTAKE_SPEED_KEY, IntakeConstants.INTAKE_SPEED));
     }
 
-    // Ensure intake is pivoted into "deployed" position, then run the intake motor in reverse to "dump" fuel out of
-    // the robot
+    // Intended for use with a press-and-hold binding
+    public Command ejectThenStopCommand()
+    {
+        return startEnd(this::eject, this::stop);
+    }
+
     public void eject()
     {
         setIntakeSpeed(
